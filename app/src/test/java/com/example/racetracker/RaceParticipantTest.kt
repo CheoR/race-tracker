9313049bf8750ea982bf66e4367c44fe7a2c0071
CheoR <cheo.roman+github@gmail.com>
2023-09-19 12:10:59 -0500

    test: race progress

--- app/src/test/java/com/example/racetracker/RaceParticipantTest.kt
+++ app/src/test/java/com/example/racetracker/RaceParticipantTest.kt
@@ -1,6 +1,10 @@
 package com.example.racetracker
 
 import com.example.racetracker.ui.RaceParticipant
+import kotlinx.coroutines.launch
+import kotlinx.coroutines.test.advanceTimeBy
+import kotlinx.coroutines.test.runCurrent
+import kotlinx.coroutines.test.runTest
 import org.junit.Test
 
 import org.junit.Assert.*
@@ -18,4 +22,41 @@ class RaceParticipantTest {
         initialProgress = 0,
         progressIncrement = 1
     )
+
+    @Test
+    fun raceParticipant_RaceStarted_ProgressUpdated() = runTest {
+        val expectedProgress = 1
+        launch { raceParticipant.run() }
+        /**
+         * Note: You can directly call the raceParticipant.run() in the runtTest builder, but the
+         * default test implementation ignores the call to delay(). As a result, the run() finishes
+         * executing before you can analyze the progress.
+
+         * The value of the raceParticipant.progressDelayMillis property determines the duration
+         * after which the race progress updates. In order to test the progress after
+         * progressDelayMillis time has elapsed, you need to add some form of delay to your test.
+         */
+        advanceTimeBy(raceParticipant.progressDelayMillis)
+
+        /**
+         * Since advanceTimeBy() doesn't run the task scheduled at the given duration, you need to
+         * call the runCurrent() function. This function executes any pending tasks at the current
+         * time.
+         */
+        runCurrent()
+
+        assertEquals(expectedProgress, raceParticipant.currentProgress)
+    }
+
+    @Test
+    fun raceParticipant_RaceFinished_ProgressUpdated() = runTest {
+        launch { raceParticipant.run() }
+
+        advanceTimeBy(raceParticipant.maxProgress * raceParticipant.progressDelayMillis)
+
+        runCurrent()
+
+        assertEquals(100, raceParticipant.currentProgress)
+    }
+
 }
