93cf26eecd2568c3a21f56e9b29f5098a9066781
CheoR <cheo.roman+github@gmail.com>
2023-09-19 09:39:37 -0500

    build: race tracker test

---
+++ app/src/test/java/com/example/racetracker/RaceParticipantTest.kt
@@ -0,0 +1,21 @@
+package com.example.racetracker
+
+import com.example.racetracker.ui.RaceParticipant
+import org.junit.Test
+
+import org.junit.Assert.*
+
+/**
+ * Example local unit test, which will execute on the development machine (host).
+ *
+ * See [testing documentation](http://d.android.com/tools/testing).
+ */
+class RaceParticipantTest {
+    private val raceParticipant = RaceParticipant(
+        name = "Test",
+        maxProgress = 100,
+        progressDelayMillis = 500L,
+        initialProgress = 0,
+        progressIncrement = 1
+    )
+}
