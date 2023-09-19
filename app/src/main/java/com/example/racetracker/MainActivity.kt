c90a478a303b46c2f168f9a558ff8e938e233f0e
CheoR <cheo.roman+github@gmail.com>
2023-09-19 09:39:15 -0500

    build: race tracker

--- app/src/main/java/com/example/racetracker/MainActivity.kt
+++ app/src/main/java/com/example/racetracker/MainActivity.kt
@@ -4,12 +4,9 @@ import android.os.Bundle
 import androidx.activity.ComponentActivity
 import androidx.activity.compose.setContent
 import androidx.compose.foundation.layout.fillMaxSize
-import androidx.compose.material3.MaterialTheme
 import androidx.compose.material3.Surface
-import androidx.compose.material3.Text
-import androidx.compose.runtime.Composable
 import androidx.compose.ui.Modifier
-import androidx.compose.ui.tooling.preview.Preview
+import com.example.racetracker.ui.RaceTrackerApp
 import com.example.racetracker.ui.theme.RaceTrackerTheme
 
 class MainActivity : ComponentActivity() {
@@ -20,27 +17,11 @@ class MainActivity : ComponentActivity() {
                 // A surface container using the 'background' color from the theme
                 Surface(
                     modifier = Modifier.fillMaxSize(),
-                    color = MaterialTheme.colorScheme.background
+//                    color = MaterialTheme.colorScheme.background
                 ) {
-                    Greeting("Android")
+                    RaceTrackerApp()
                 }
             }
         }
-    }
-}
-
-@Composable
-fun Greeting(name: String, modifier: Modifier = Modifier) {
-    Text(
-        text = "Hello $name!",
-        modifier = modifier
-    )
-}
-
-@Preview(showBackground = true)
-@Composable
-fun GreetingPreview() {
-    RaceTrackerTheme {
-        Greeting("Android")
     }
 }
